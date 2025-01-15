b1bf7d21ba3735742c66a2baa571d7d09260432f
JoseAR68 <jose.reygarcia@gmail.com>
2025-01-15 12:57:46 +0100

    Delete S2_01_NIVEL2_EJERCICIO2.sql

--- S2_01_NIVEL2_EJERCICIO2.sql
+++
@@ -1,8 +0,0 @@
--- Quina és la mitjana de vendes per país? Presenta els resultats ordenats de major a menor mitjà.
-
-SELECT country, AVG(amount) AS media_ventas
-FROM company
-JOIN transaction ON transaction.company_id = company.id
-GROUP BY country
-ORDER BY media_ventas DESC
-;
